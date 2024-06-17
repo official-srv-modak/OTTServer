b3e1337acd6177e1490d85302b0d391b5b66cf06
Sourav Modak <72427070+official-srv-modak@users.noreply.github.com>
2024-06-17 16:29:15 -0400

    Update path.php

--- path.php
+++ path.php
@@ -1,8 +1,10 @@
 <?php
+    $server_ip = gethostbyname(gethostname());
+
     $path_movies = "path_movies.txt";
     $path_tv = "path_tv.txt";
     $path_cached_images = "path_cached_images.txt";
-    $path_cached_images_ip = "http://www.modakflix.com/cached_images/";
-    $path_server = "http://www.modakflix.com/";
-	$path_server_root = "C:/xampp/htdocs/";
+    $path_cached_images_ip = "http://{$server_ip}/cached_images/";
+    $path_server = "http://{$server_ip}/";
+    $path_server_root = "C:/xampp/htdocs/";
 ?>
