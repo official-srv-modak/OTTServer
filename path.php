bcca28ee0752ab37cabd69577dc240f2b808841d
Sourav Modak <72427070+official-srv-modak@users.noreply.github.com>
2024-10-13 21:24:59 -0400

    Update path.php

--- path.php
+++ path.php
@@ -1,10 +1,10 @@
 <?php
-    $server_ip = gethostbyname(gethostname());
+    $server_ip = trim(shell_exec("ifconfig | grep 'inet ' | grep -v 127.0.0.1 | awk '{print $2}'"));
 
     $path_movies = "path_movies.txt";
     $path_tv = "path_tv.txt";
     $path_cached_images = "path_cached_images.txt";
-    $path_cached_images_ip = "http://{$server_ip}/cached_images/";
-    $path_server = "http://{$server_ip}/";
-    $path_server_root = "/Users/souravmodak/Documents/projects/OTTServer1/";
+    $path_cached_images_ip = "http://$server_ip/cached_images/";
+    $path_server = "http://$server_ip/";
+    $path_server_root = "/Applications/XAMPP/xamppfiles/htdocs/";
 ?>
